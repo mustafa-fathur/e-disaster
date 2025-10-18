3428d36f385d5fe4b099f7fd688262e4dc8adb31
Habiboys <nouvalhabibie18@gmail.com>
2025-10-18 21:32:19 +0700

    halaman home & daftar bencana

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.compose.foundation)
+    implementation("io.coil-kt:coil-compose:2.7.0")
+
 
     // Networking dependencies
     implementation(libs.retrofit)
--- app/src/main/java/com/example/e_disaster/ui/screens/AddDisasterScreen.kt
+++ app/src/main/java/com/example/e_disaster/ui/screens/AddDisasterScreen.kt
@@ -33,3 +33,4 @@ fun AddDisasterScreen(navController: NavController) {
         }
     }
 }
+
